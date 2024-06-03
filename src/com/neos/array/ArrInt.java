54b57b457d1d74367ae10df353960c3afa412109
lathieuhiep <khacdiepkma90@gmail.com>
2024-06-03 16:56:13 +0700

    update code

---
+++ src/com/neos/array/ArrInt.java
@@ -0,0 +1,9 @@
+package com.neos.array;
+
+public class ArrInt {
+    private int[] arrInt;
+
+    public ArrInt() {
+        arrInt = new int[] {1, 2, 3, 4, 5, 2, 3, 1, 5, 4, 1, 4, 5, 3, 2};
+    }
+}
