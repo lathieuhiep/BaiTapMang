21b045c439c7a3cb94d3dbbb3ace136f570e59cc
lathieuhiep <khacdiepkma90@gmail.com>
2024-06-23 22:28:54 +0700

    update code

--- src/com/neos/array/ArrInt.java
+++ src/com/neos/array/ArrInt.java
@@ -4,7 +4,7 @@ public class ArrInt {
     private int[] arrInt;
 
     public ArrInt() {
-        arrInt = new int[] {1, 2, 3, 4, 5, 2, 3, 1, 5, 4, 1, 4, 5, 3, 2};
+        arrInt = new int[] {1, 2, 3, 4, 5, 2, 9, 3, 1, 5, 4, 1, 9, 4, 5, 3, 2};
     }
 
     // kiểm tra số nguyên tố
